59f3f84d151c8374e4445bec189e3d47f7c40a64
ja54312 <ija54312@gmail.com>
2023-02-07 13:58:15 -0600

    acutalizacion

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //base: "/" para dominios personales
   base: "https://ja54312.github.io/gh-actions-react/",
   plugins: [react()],
 });
